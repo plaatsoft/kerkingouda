dc865966d473932046fabe14f7df8fecda819f60
wplaat <wplaat wplaat@gmail.com>
2020-03-20 21:40:52 +0100

    Initial checkin

---
+++ app/src/main/java/nl/plaatsoft/kerkingouda/AboutActivity.java
@@ -0,0 +1,10 @@
+package nl.plaatsoft.kerkingouda;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
